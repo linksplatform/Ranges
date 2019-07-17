b34c61f255ad59696c132b60a02616a7b2939b28
Константин Дьяченко <konard@me.com>
2019-07-17 18:19:04 +0300

    Closes #1

--- push-nuget.bat
+++ push-nuget.bat
@@ -1,8 +1,6 @@
-dotnet pack -c Release --include-symbols
-cd bin\Release
-dotnet nuget push -s https://api.nuget.org/v3/index.json *.symbols.nupkg
-del *.symbols.nupkg
-dotnet nuget push -s https://api.nuget.org/v3/index.json *.nupkg
+dotnet pack -c Release
+cd bin\Release\
+nuget push -Source https://api.nuget.org/v3/index.json *.nupkg
 del *.nupkg
-cd ..
-cd ..
+del *.snupkg
+cd ..\..
