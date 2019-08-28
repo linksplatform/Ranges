589b6909ec03933ce20d0fbc123b65197d5499af
Константин Дьяченко <konard@me.com>
2019-08-28 22:23:05 +0300

    Travis CI configuration updated to the latest version.

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
+if [[ ( "$TRAVIS_PULL_REQUEST" != "false" ) || ( "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ) ]]; then
+    echo "Skipping documentation deploy."
     exit 0
 fi
 
-# Save some useful information
-SHA=`git rev-parse --verify HEAD`
-COMMIT_AUTHOR_EMAIL="konard@me.com"
-REPOSITORY="github.com/linksplatform/$TRAVIS_REPO_NAME"
+# Settings
+TARGET_BRANCH="gh-pages"
+SHA=$(git rev-parse --verify HEAD)
+COMMIT_USER_NAME="linksplatform-docs"
+COMMIT_USER_EMAIL="konard@yandex.ru"
+REPOSITORY="github.com/linksplatform/${TRAVIS_REPO_NAME}"
+
+# Insert repository name into DocFX's configuration files
+sed -i "s/\$TRAVIS_REPO_NAME/${TRAVIS_REPO_NAME}/g" toc.yml
+sed -i "s/\$TRAVIS_REPO_NAME/${TRAVIS_REPO_NAME}/g" docfx.json
 
 # DocFX installation
 nuget install docfx.console
-mono $(ls | grep "docfx.console.")/tools/docfx.exe docfx.json
+mono $(echo ./*docfx.console.*)/tools/docfx.exe docfx.json
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone https://$REPOSITORY out
-cd out
+cd out || exit
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
@@ -32,16 +35,16 @@ rm -rf out/**/* || exit 0
 # Copy genereted docs site
 cp -r _site/* out
 
-cd out
+cd out || exit
 
 # Do not use index.md
 cp README.html index.html
 
 # Now let's go have some fun with the cloned repo
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.name "$COMMIT_USER_NAME"
+git config user.email "$COMMIT_USER_EMAIL"
 git remote rm origin
-git remote add origin https://linksplatform-docs:$TOKEN@$REPOSITORY.git
+git remote add origin https://$COMMIT_USER_NAME:$TOKEN@$REPOSITORY.git
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -49,4 +52,4 @@ git add --all
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Now that we're all set up, we can push.
-git push https://linksplatform-docs:$TOKEN@$REPOSITORY.git $TARGET_BRANCH
+git push https://$COMMIT_USER_NAME:$TOKEN@$REPOSITORY.git $TARGET_BRANCH
