e43bb21933ce44cc280b0a226fc7773fc2f6013a
Konstantin Diachenko <drakonard@gmail.com>
2020-03-07 07:05:42 +0300

    New version of translator applied to tests.

---
+++ cpp/Platform.Ranges.Tests/EnsureExtensionsTests.cpp
@@ -0,0 +1,9 @@
+﻿namespace Platform::Ranges::Tests
+{
+    TEST_CLASS(EnsureExtensions)
+    {
+        public: TEST_METHOD(MaximumArgumentIsGreaterOrEqualToMinimumExceptionTest) { Assert::ExpectException<ArgumentException>([&]()-> auto { return Platform::Ranges::EnsureExtensions::MaximumArgumentIsGreaterOrEqualToMinimum(Platform::Exceptions::Ensure::Always, 2, 1); }); }
+
+        public: TEST_METHOD(ArgumentInRangeExceptionTest) { Assert::ExpectException<ArgumentOutOfRangeException>([&]()-> auto { return Platform::Ranges::EnsureExtensions::ArgumentInRange(Platform::Exceptions::Ensure::Always, 5, (6, 7)); }); }
+    };
+}
---
+++ cpp/Platform.Ranges.Tests/RangeTests.cpp
@@ -0,0 +1,49 @@
+﻿namespace Platform::Ranges::Tests
+{
+    TEST_CLASS(Range)
+    {
+        public static void ConstructorsTest()
+        {
+            auto range1 = new Range<std::int32_t>(1, 3);
+            Assert::AreEqual(1, range1.Minimum);
+            Assert::AreEqual(3, range1.Maximum);
+            Assert::ExpectException<ArgumentException>([&]()-> auto { return new Range<std::int32_t>(2, 1); });
+            Range<std::int32_t, 5> range2;
+            Assert::AreEqual(5, range2.Minimum);
+            Assert::AreEqual(5, range2.Maximum);
+        }
+
+        public: TEST_METHOD(ContainsTest)
+        {
+            auto range = new Range<std::int32_t>(1, 3);
+            Assert.True(range.Contains(1));
+            Assert.True(range.Contains(2));
+            Assert.True(range.Contains(3));
+            Assert.True(range.Contains((2, 3)));
+            Assert.False(range.Contains((3, 4)));
+        }
+
+        public: TEST_METHOD(DifferenceTest)
+        {
+            auto range = new Range<std::int32_t>(1, 3);
+            Assert::AreEqual(2, range.Difference());
+        }
+
+        public: TEST_METHOD(ToStringTest)
+        {
+            auto range = new Range<std::int32_t>(1, 3);
+            Assert::AreEqual("[1, 3]", range.ToString());
+        }
+
+        public: TEST_METHOD(EqualityTest)
+        {
+            auto range1 = new Range<std::int32_t>(1, 3);
+            auto range1Duplicate = new Range<std::int32_t>(1, 3);
+            auto range2 = new Range<std::int32_t>(2, 5);
+            Assert.True(range1 == range1Duplicate);
+            Assert::AreEqual(range1, range1Duplicate);
+            Assert.True(range1 != range2);
+            Assert.NotEqual(range1, range2);
+        }
+    };
+}
