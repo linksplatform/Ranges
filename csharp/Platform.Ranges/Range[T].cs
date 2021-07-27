404cf94707595b101103cec70bc3750804ed8bf7
FreePhoenix <freephoenix888@gmail.com>
2021-07-28 00:33:04 +0600

    Update XML Docs

--- csharp/Platform.Ranges/Range[T].cs
+++ csharp/Platform.Ranges/Range[T].cs
@@ -81,38 +81,38 @@ namespace Platform.Ranges
         public override string ToString() => $"[{Minimum}..{Maximum}]";
 
         /// <summary>
-        /// <para>Determines if the <paramref name="value"/> is in the <see cref="Range{T}"/>.</para>
-        /// <para>Определяет, находится ли <paramref name="value"/> в <see cref="Range{T}"/>.</para>
+        /// <para>Determines if the <see cref="Range{T}"/> contains the <paramref name="value"/>.</para>
+        /// <para>Определяет, содержит ли <see cref="Range{T}"/> <paramref name="value"/>.</para>
         /// </summary>
         /// <param name="value">
         /// <para>A value to find in the <see cref="Range{T}"/>.</para>
         /// <para>Значение, которое нужно найти в <see cref="Range{T}"/>.</para>
         /// </param>
         /// <returns>
-        /// <para>A <see cref="Boolean"/> value that determines whether the <paramref name="value"/> is in the current <see cref="Range{T}"/>.</para>
-        /// <para>Значение типа <see cref="Boolean"/>, определяющее находится ли <paramref name="value"/> в текущем <see cref="Range{T}"/>.</para>
+        /// <para>A <see cref="Boolean"/> value that determines whether the <see cref="Range{T}"/> contains the <paramref name="value"/>.</para>
+        /// <para>Значение типа <see cref="Boolean"/>, определяющее содержит ли <see cref="Range{T}"/> <paramref name="value"/>.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Contains(T value) => _comparer.Compare(Minimum, value) <= 0 && _comparer.Compare(Maximum, value) >= 0;
 
         /// <summary>
-        /// <para>Determines if the <paramref name="range"/> is in the current <see cref="Range{T}"/>.</para>
-        /// <para>Определяет, находится ли <paramref name="range"/> в текущем <see cref="Range{T}"/>.</para>
+        /// <para>Determines if the current <see cref="Range{T}"/> contains the <paramref name="range"/>.</para>
+        /// <para>Определяет, содержит ли текущий <see cref="Range{T}"/> <paramref name="range"/>.</para>
         /// </summary>
         /// <param name="range">
         /// <para>A <see cref="Range{T}"/> to find in the current <see cref="Range{T}"/>.</para>
         /// <para><see cref="Range{T}"/>, который нужно найти в текущем <see cref="Range{T}"/>.</para>
         /// </param>
         /// <returns>
-        /// <para>A <see cref="Boolean"/> value that determines whether the <paramref name="range"/> is in the current <see cref="Range{T}"/>.</para>
-        /// <para>Значение типа <see cref="Boolean"/>, определяющее, находится ли <paramref name="range"/> в текущем<see cref="Range{T}"/>.</para>
+        /// <para>A <see cref="Boolean"/> value that determines whether the current <see cref="Range{T}"/> contains the <paramref name="range"/>.</para>
+        /// <para>Значение типа <see cref="Boolean"/>, определяющее, содержит ли текущий <see cref="Range{T}"/> <paramref name="range"/>.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Contains(Range<T> range) => Contains(range.Minimum) && Contains(range.Maximum);
 
         /// <summary>
-        /// <para>Determines whether the <paramref name="other"/> and the current <see cref="Range{T}"/> are equal.</para>
-        /// <para>Определяет, равны ли <paramref name="other"/> и текущий <see cref="Range{T}"/>.</para>
+        /// <para>Determines whether the current <see cref="Range{T}"/> and the <paramref name="other"/> are equal.</para>
+        /// <para>Определяет, равны ли текущий <see cref="Range{T}"/> и <paramref name="other"/>.</para>
         /// </summary>
         /// <param name="other"><para>A <see cref="Range{T}"/> to compare with the current <see cref="Range{T}"/>.</para><para><see cref="Range{T}"/> для сравнения с текущим <see cref="Range{T}"/>.</para></param>
         /// <returns>
